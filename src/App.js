ca8198bd6c31c85f1930eb557e80c53e019eccdf
Marcus Vinicius da Silva Borges <marcus.f22@gmail.com>
2019-05-02 00:14:28 -0300

    adds Dashboard to basic layout of App.

--- src/App.js
+++ src/App.js
@@ -4,11 +4,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import './App.css';
 import NavBar from './components/layout/NavBar';
+import Dashboard from './components/layout/Dashboard';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
+      <div className="container">
+        <Dashboard/>
+      </div>
     </div>
   );
 }
