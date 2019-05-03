3d38f9566a711b4412a000e033559dde8e6b80bd
Marcus Vinicius da Silva Borges <marcus.f22@gmail.com>
2019-05-03 11:31:07 -0300

    adds links to list all pokemon and to create a new one.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -8,6 +8,7 @@ import NavBar from './components/layout/NavBar';
 import Dashboard from './components/layout/Dashboard';
 
 import Pokemon from './components/pokemon/Pokemon';
+import PokemonForm from './components/pokemon/PokemonForm';
 
 function App() {
   return (
@@ -16,7 +17,9 @@ function App() {
         <NavBar/>
         <div className="container">
           <Switch>
-            <Route exact path="/" component={Dashboard}></Route>
+            <Route exact path="/"><Redirect to="/pokemon"/></Route>
+            <Route exact path="/pokemon" component={Dashboard}></Route>
+            <Route exact path="/pokemon/new" component={PokemonForm}></Route>
             <Route exact path="/pokemon/:id" component={Pokemon}></Route>
           </Switch>
         </div>
--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -1,18 +1,44 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
 export default class NavBar extends Component {
   render() {
     return (
       <div>
         <nav className="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-          <a 
-            href='#'
+          <Link 
+            to="/pokemon"
             className="navbar-brand col-sm-3 col-md-2 mb-0 align-items-center"
           >
             Pokédex
-          </a>
+          </Link>
+          <div className="collape navbar-collapse">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <ItemLink
+                  to="/pokemon/new"
+                  className="col-sm-3 col-md-2 mb-0 align-items-center"
+                >
+                  Create Pokémon
+                </ItemLink>
+              </li>
+            </ul>
+          </div>
         </nav>
       </div>
     )
   }
 }
+
+const ItemLink = styled(Link)`
+  text-decoration: none;
+  color: white;
+  &:focus,
+  &:hover,
+  &:visited,
+  &:link,
+  &:active {
+    text-decoration: none;
+  }
+`;
