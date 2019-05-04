d5f8435a945e2a056db4f89b54f9c199ab1378cc
Marcus Vinicius da Silva Borges <marcus.f22@gmail.com>
2019-05-03 22:17:05 -0300

    updates PokemonForm so it can create and update a pokemon correctly.

--- src/components/pokemon/PokemonForm.js
+++ src/components/pokemon/PokemonForm.js
@@ -6,27 +6,31 @@ export default class PokemonForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      id: '',
       name: '',
-      index: '',
       sprite: '',
-      typeIds: [],
+      selectedTypes: [],
+      selectedEvolutions: [],
       typeOptions: [],
       pokemonOptions: [],
-      evolutionIds: [],
     };
   }
 
+  componentDidMount = async () => {
+    const { id } = this.props.match.params;
+    if (id) {
+      const data = await this.fetchCurrentPokemon(id);
+      this.fillFormWithCurrentPokemon(data);
+    }
+    await this.fillFormOptions();
+  }
+
   handleTypesChange = (selectedTypes) => {
-    this.setState({ typeIds: selectedTypes.map((type) => type.id) });
+    this.setState({ selectedTypes });
   }
 
   handleEvolutionsChange = (selectedEvolutions) => {
-    this.setState({ evolutionIds: selectedEvolutions.map((evolution) => evolution.id) });
-  }
-
-  componentDidMount = async () => {
-    this.setState({ typeOptions: await this.fetchFromAPI('types') });
-    this.setState({ pokemonOptions: await this.fetchFromAPI('pokemon') });
+    this.setState({ selectedEvolutions });
   }
 
   fetchFromAPI = async (resource) => {
@@ -35,22 +39,77 @@ export default class PokemonForm extends Component {
     return data;
   }
 
-  savePokemon = async (event) => {
-    event.preventDefault();
+  fetchCurrentPokemon = async (id) => {
+    const url = `http://localhost:4000/v1/pokemon/${id}`;
+    const { data } = await axios.get(url);
+    return data;
+  }
+
+  fillFormOptions = async() => {
+    this.setState({ typeOptions: await this.fetchFromAPI('types') });
+    this.setState({ pokemonOptions: await this.fetchFromAPI('pokemon') });
+  }
+
+  fillFormWithCurrentPokemon = async(data) => {
+    const { name, types } = data;
+    const evolutions = data.evolves_to;
+
+    this.setState({
+      name,
+      selectedTypes: types,
+      selectedEvolutions: evolutions,
+    });
+  }
+
+  getTypeIds = () => this.state.selectedTypes.map(type => type.id);
+  
+  getEvolutionIds = () => this.state.selectedEvolutions.map(evolution => evolution.id);
+
+  updatePokemon = async (id) => {
+    const url = `http://localhost:4000/v1/pokemon/${id}`;
+    try {
+      await axios.put(url, {
+        name: this.state.name,
+        type_ids: this.getTypeIds(),
+        evolution_ids: this.getEvolutionIds(),
+        sprite: this.state.sprite,
+      });
+      alert(`${this.state.name} updated sucessfully.`);
+      this.props.history.push(`/pokemon/${id}`);
+    } catch (e) {
+      alert('Error!' + e.message)
+    }
+  }
+
+  createPokemon = async () => {
     const url = 'http://localhost:4000/v1/pokemon';
     try {
       const { data } = await axios.post(url, {
         name: this.state.name,
-        type_ids: this.state.typeIds,
-        evolution_ids: this.state.evolutionIds,
+        type_ids: this.getTypeIds(),
+        evolution_ids: this.getEvolutionIds(),
         sprite: this.state.sprite,
       });
       alert(`${data.name} created sucessfully.`);
       this.props.history.push(`/pokemon/${data.id}`);
     } catch (e) {
-      console.log(e)
       alert('Error!' + e.message);
     }
+  }
+
+  savePokemon = async (event) => {
+    event.preventDefault();
+    const { id } = this.props.match.params;
+
+    if (id) {
+      this.updatePokemon(id);
+    } else {
+      this.createPokemon();
+    }
+  }
+
+  renderProperTitle = () => {
+    return this.props.match.params.id ? <h5>Edit Pokémon</h5> : <h5>Create Pokémon</h5>;
   }
 
   render() {
@@ -58,7 +117,7 @@ export default class PokemonForm extends Component {
       <div className="col">
         <div className="card">
           <div className="card-header">
-            <h5>Create Pokemon</h5>
+            { this.renderProperTitle() }
           </div>
           <div className="card-body">
             <form onSubmit={this.savePokemon}>
@@ -70,35 +129,27 @@ export default class PokemonForm extends Component {
                 />
               </div>
               <br />
-
-              <div>
-                <label>Index:</label>
-                <input 
-                  value={this.state.index}
-                  onChange={e => this.setState({ index: e.target.value })}
-                />
-              </div>
-              <br />
-              
               <div>
                 <label>Types:</label>
                 <Select
                   isSearchable={true}
                   options={this.state.typeOptions} 
                   isMulti={true}
+                  value={this.state.selectedTypes}
                   getOptionLabel={(option) => option.name}
                   getOptionValue={(option) => option.id}
                   onChange={this.handleTypesChange}
                 />
               </div>
               <br />
-
+            
               <div>
                 <label>Evolves to:</label>
                 <Select
                   isSearchable={true}
                   options={this.state.pokemonOptions} 
                   isMulti={true}
+                  value={this.state.selectedEvolutions}
                   getOptionLabel={(option) => option.name}
                   getOptionValue={(option) => option.id}
                   onChange={this.handleEvolutionsChange}
