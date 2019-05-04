1bd51b611286b577d73b0bf44949bd92f853ea46
Marcus Vinicius da Silva Borges <marcus.f22@gmail.com>
2019-05-03 21:19:46 -0300

    adds search bar to PokemonList.js.

--- src/components/pokemon/PokemonList.js
+++ src/components/pokemon/PokemonList.js
@@ -1,17 +1,28 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import Search from 'react-search';
 
 import PokemonCard from './PokemonCard';
 
 export default class PokemonList extends Component {
   state = {
-    url: "http://localhost:4000/v1/pokemon",
-    pokemons: null
+    pokemons: [],
   };
 
-  async componentDidMount() {
-    const response = await axios.get(this.state.url);
-    this.setState({ pokemons: response.data })
+  componentDidMount = async () => {
+    const { data } = await axios.get('http://localhost:4000/v1/pokemon');
+    this.setState({ pokemons: data });
+  }
+
+  getAsyncPokemons = async (searchName) => {
+    const url = `http://localhost:4000/v1/pokemon?name=${searchName}`;
+    const { data } = await axios.get(url);
+    try {
+      let foo = data.map((pokemon) => { return { id: pokemon.id, name: pokemon.name }})
+      this.setState({ pokemons: data });
+    } catch (e) {
+      alert('Error!' + e);
+    }
   }
   
   render() {
@@ -19,14 +30,23 @@ export default class PokemonList extends Component {
       <React.Fragment>
         { this.state.pokemons ? (
           <div className="row">
-            {this.state.pokemons.map(pokemon => (
-              <PokemonCard
-                key={pokemon.id}
-                id={pokemon.id}
-                name={pokemon.name}
-                spriteUrl={pokemon.sprite_url}
+            <div className="rol">
+              <Search
+                items={this.state.pokemons}
+                getItemsAsync={this.getAsyncPokemons}
+                placeholder="Search a pokemon"
               />
-            ))}
+            </div>
+            <div className="row">
+              {this.state.pokemons.map(pokemon => (
+                <PokemonCard
+                  key={pokemon.id}
+                  id={pokemon.id}
+                  name={pokemon.name}
+                  spriteUrl={pokemon.sprite_url}
+                />
+              ))}
+            </div>
           </div>) : (
             <h1>Loading Pokemon</h1>
           )
